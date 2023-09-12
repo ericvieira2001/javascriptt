e6d1e5db729c4db76b48d627344134c546bf94fb
ericvieira2001 <vieiraeiro@gmail.com>
2023-09-12 17:50:55 -0300

    ..

---
+++ ex08/node.js
@@ -0,0 +1,6 @@
+var velo = 77
+console.log(`sua velocidade é de ${velo}km/h`)
+if( velo > 70){
+    console.log("você foi multado(a) por velocidade acima do permitido")
+}
+console.log("sempre faça o uso do cinto de segurança!")
---
+++ ex09/node.js
@@ -0,0 +1,9 @@
+var pais = 'sil'
+console.log(`nascido(a) no(a) ${pais}`)
+
+if( pais == 'Brasil'){
+    console.log("você é brasileiro(a)")
+}else{
+    console.log(" você é estrangeiro(a)")
+}
+
